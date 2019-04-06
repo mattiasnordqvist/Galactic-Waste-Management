5600e4d907b5db64b070f28944e8f50dd8e9ea72
Mattias Nordqvist <mattias.nordqvist@gmail.com>
2019-04-06 13:22:11 +0200

    NoColor as parameter to ConsoleLogger

--- GalacticWasteManagement/Logging/ConsoleLogger.cs
+++ GalacticWasteManagement/Logging/ConsoleLogger.cs
@@ -6,6 +6,7 @@ namespace GalacticWasteManagement.Logging
 {
     public class ConsoleLogger : ILogger
     {
+        public bool NoColor { get; set; } = false;
         ArtisticPainter painter = new ArtisticPainter();
         public ConsoleLogger(string databaseName)
         {
@@ -15,18 +16,25 @@ namespace GalacticWasteManagement.Logging
         }
         public void Log(string message, string type)
         {
-            var defaultBrush = type == "unicorn"
-                ? (IArtisticBrush)new RainbowBrush()
-                : type == "success"
-                ? new BasicBrush(Color.YellowGreen)
-                : type == "warning"
-                ? new BasicBrush(Color.Yellow)
-                : type == "error"
-                ? new BasicBrush(Color.Red)
-                : type == "important"
-                ? new BasicBrush(Color.White)
-                : new BasicBrush(Color.DimGray);
-            Console.WriteLine(painter.Unleash(new ArtisticString(message, defaultBrush)));
+            if (NoColor)
+            {
+                Console.WriteLine($"{message}");
+            }
+            else
+            {
+                var defaultBrush = type == "unicorn"
+                    ? (IArtisticBrush)new RainbowBrush()
+                    : type == "success"
+                    ? new BasicBrush(Color.YellowGreen)
+                    : type == "warning"
+                    ? new BasicBrush(Color.Yellow)
+                    : type == "error"
+                    ? new BasicBrush(Color.Red)
+                    : type == "important"
+                    ? new BasicBrush(Color.White)
+                    : new BasicBrush(Color.DimGray);
+                Console.WriteLine(painter.Unleash(new ArtisticString(message, defaultBrush)));
+            }
         }
     }
 }
